64624636522851694679610874baba4224dbf4d8
py-task <2529716798@qq.com>
2018-02-23 15:43:46 +0800

    基于swagger2简化文档

--- src/main/java/com/example/blog/controller/UserController.java
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.blog.controller;
 
 import com.example.blog.entity.User;
 import com.example.blog.util.ResultMessage;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -11,23 +14,32 @@ import java.util.*;
 public class UserController {
     public static Map<Integer, User> users = Collections.synchronizedMap(new HashMap<>());
 
+    @ApiOperation(value="获取用户列表", notes="")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getUserList(){
         ArrayList<User> list = new ArrayList(users.values());
         return list;
     }
 
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResultMessage postUser(@ModelAttribute User user){
         users.put(user.getId(), user);
         return ResultMessage.Success;
     }
 
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "int")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable int id){
         return users.get(id);
     }
 
+
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResultMessage putUser(@PathVariable int id, @ModelAttribute User user){
         User u = users.get(id);
