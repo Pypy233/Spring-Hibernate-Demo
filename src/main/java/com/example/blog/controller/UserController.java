b98223eff0b413c283edfb8ef09d11dadc0e43f3
py-task <2529716798@qq.com>
2018-02-23 15:46:50 +0800

    修正注解错位

--- src/main/java/com/example/blog/controller/UserController.java
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -38,8 +38,11 @@ public class UserController {
 
 
 
-    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "")
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResultMessage putUser(@PathVariable int id, @ModelAttribute User user){
         User u = users.get(id);
@@ -49,6 +52,8 @@ public class UserController {
         return ResultMessage.Success;
     }
 
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "int")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResultMessage deleteUser(@PathVariable int id){
         users.remove(id);
